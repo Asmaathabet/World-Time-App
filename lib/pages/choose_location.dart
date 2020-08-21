a97f0d86c79a4764dde3297a98bdc9127966bc35
Asmaathabet <asmathabet2009@hotmail.com>
2020-08-21 20:52:39 +0300

    add simple choose location page:

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -9,6 +9,13 @@ class _ChooseLocationState extends State<ChooseLocation> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        backgroundColor: Colors.blue[900],
+        centerTitle: true,
+        title: Text('choose a Location'),
+        elevation: 0,
+      ),
       body: Text('Choose Location screen'),
     );
   }
