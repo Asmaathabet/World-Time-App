ded44999380863c2d12f2601ffab8488535bcedd
Asmaathabet <asmathabet2009@hotmail.com>
2020-08-22 23:25:52 +0300

    changes background image and colors depending on day and night

--- lib/services/world_time.dart
+++ lib/services/world_time.dart
@@ -7,6 +7,7 @@ class WorldTime {
   String time;      // time for that location
   String flag;     // url to an asset flag icon
   String url;      //location url for api endpoint
+  bool isDayTime;  // true or false for being day time
 
   WorldTime({this.location, this.flag, this.url});
 
@@ -27,7 +28,8 @@ class WorldTime {
       now = now.add(Duration(hours: int.parse(offset)));
       // set the time property
 //      time = now.toString();
-    time = DateFormat.jm().format(now);
+     isDayTime = now.hour > 6 && now.hour < 20 ? true : false ;
+      time = DateFormat.jm().format(now);
     }
     catch(e){
      print('caught error: $e');
